e357d21a009a1ff99cdfbebade940938e105cb45
jguerreiro <joel.borghi@gmail.com>
2025-01-15 21:10:38 -0300

    Refactoring code and trying to run the debugger

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -2,10 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 
-#define DEST_SIZE 40
-
-/* 
-
+/*
 Mario-less pyramid left example, height 8:
 
        # 1.
@@ -29,38 +26,18 @@ Mario-less pyramid right example, height 8:
 8. ########
 
 */
-
-int printSpace(int value)
-{
-    // Ex: spaces [2] = [" ", " "]; 
-
-
-    for (int i = 0; i < value; i++) 
-    {
-        //spaces[i] = "*";
-        //printf("%s", spaces[i]);
-        
-    }
-
-    return 0;  
-}
-
-int printSpace(int value);
 int main (void)
 {
     // Mario's pyramid's height.
-    int pyramidSize = get_int("What is the size of Mario's pyramid? ");
-    string spaces[pyramidSize];
+    int pyramidSize = 0;
+    pyramidSize = get_int("What is the size of Mario's pyramid? ");
     
     for (int i = 0; i <= pyramidSize; i++) 
     {
         for (int j = 0; j < i; j++)
         {
-            //int space = printSpace(i);
             printf("#");
         }
         printf("\n");
     }
-
-
 }
